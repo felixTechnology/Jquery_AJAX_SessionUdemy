3cdb7344f7a5b6f21fe285ea25136a3173525931
Felix Atsu Kofi Ackuayi <felixackuayi@aol.com>
2020-10-10 08:38:35 +0000

    1.Learn what is JQUERY..2. why it's good to work with this Library...3, learnt about selection (search for element in entire html,DOM Element,search by ID,CLASS, get them and highlight) 4. Learnt traversing (finding element which is efficient than CSS selectors,work with parent,ceiling) 5. Learnt event (click,remove entries,read info,etc) 5. animation 6.Manupilate DOM

--- js/main.js
+++ js/main.js
@@ -334,6 +334,30 @@ function addItems (id, name, description ,price, moreInfo ){
         $('#newsletter-checkbox').trigger('change');
 
 
+        $('#cart-form').on('submit',function (event) {
+          event.preventDefault();
+
+          let data = { form: $(this).serialize(),
+          price: cart
+          };
+
+          console.log(data.form);
+
+          $.ajax($(this).attr('action'),{
+              type: 'post',
+              data: data
+
+
+          })
+          .done(function (response) {
+
+              $('#feedback-message').text(response.message);
+            //alert(response.message);
+              //alert(data)
+              alert(data.form + ':'+ response.message);
+            })
+        });
+
     })
 
 //});
